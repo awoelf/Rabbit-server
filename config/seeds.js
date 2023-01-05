b5db90cb71aeeef0b22e9b6604f3fcce628adc09
awoelf <98348782+awoelf@users.noreply.github.com>
2023-01-04 20:08:01 -0600

    copied files

---
+++ config/seeds.js
@@ -0,0 +1,27 @@
+const db = require('./connection');
+const { User } = require('../models');
+
+db.once('open', async () => {
+  
+
+  await User.deleteMany();
+
+  await User.create({
+    firstName: 'Yeon',
+    lastName: 'Seo',
+    email: 'yeon@me.com',
+    password: 'password12345',
+   
+  });
+
+  await User.create({
+    firstName: 'Elijah',
+    lastName: 'Holt',
+    email: 'eholt@testmail.com',
+    password: 'password12345'
+  });
+
+  console.log('users seeded');
+
+  process.exit();
+});
