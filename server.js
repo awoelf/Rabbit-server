8bc576c48becddfb66d394800828e991f2202eb3
awoelf <98348782+awoelf@users.noreply.github.com>
2023-01-04 20:53:58 -0600

    changed server file

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
@@ -7,37 +10,42 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+// const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
-app.get('/', (req, res) => {
-  res.json('Hello');
-});
+// app.get('/', (req, res) => {
+//   res.json('Hello');
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
+// const startApolloServer = async (typeDefs, resolvers) => {
+//   await server.start();
+//   server.applyMiddleware({ app });
   
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at https://rabbit-app.herokuapp.com/${PORT}${server.graphqlPath}`);
-    })
-  })
- };
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at https://rabbit-app.herokuapp.com/${PORT}${server.graphqlPath}`);
+//     })
+//   })
+//  };
   
   // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
+  // startApolloServer(typeDefs, resolvers);
 
+const { url } = await startStandaloneServer(server, {
+  listen: { port: PORT },
+});
+
+console.log(`Server ready at ${url}`);
