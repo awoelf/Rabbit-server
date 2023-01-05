b79d3e528afa73155f630e004deaebb7f427f3cd
awoelf <98348782+awoelf@users.noreply.github.com>
2023-01-04 20:24:14 -0600

    replaced single quotes with `

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@rabbit-app.dsfvpug.mongodb.net/rabbit', {
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@rabbit-app.dsfvpug.mongodb.net/rabbit`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
