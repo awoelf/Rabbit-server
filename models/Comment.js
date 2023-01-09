d542a3c4615b650946e6a6748695ac3d75460498
awoelf <98348782+awoelf@users.noreply.github.com>
2023-01-09 14:49:14 -0600

    removed stripe and added post and comment schemas

---
+++ models/Comment.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+    commentText: {
+        type: String,
+        required: true
+    },
+    commentAuthor: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    }
+});
+
+module.exports = commentSchema;
