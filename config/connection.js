344be7533b46141fbf2afd9b2ca3b3417f2c8540
rogseo <rogseo@gmail.com>
2023-02-01 23:21:03 -0600

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 
 
 
-// mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@rabbit-app.dsfvpug.mongodb.net/rabbit`, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://curriculum-admin:${process.env.MONGODB_PASSWORD}@cluster0.umgm8xe.mongodb.net/rabbit`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@rabbit-app.dsfvpug.mongodb.net/rabbit`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://curriculum-admin:${process.env.MONGODB_PASSWORD}@cluster0.umgm8xe.mongodb.net/rabbit`, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 module.exports = mongoose.connection;
