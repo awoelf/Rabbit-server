95c02aef6d946547e232571c9929cd96d44c8da3
awoelf <98348782+awoelf@users.noreply.github.com>
2023-03-08 17:50:04 -0600

    added prettier and error logs

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -4,93 +4,93 @@ const { signToken } = require('../utils/auth');
 const bcrypt = require('bcrypt');
 
 const resolvers = {
-	Query: {
-		user: async (parent, args, context) => {
-			if (context.user) {
-				const user = await User.findById(context.user._id).populate('friends');
+  Query: {
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate('friends');
 
-				return user;
-			}
+        return user;
+      }
 
-			throw new AuthenticationError('Not logged in');
-		},
-		allUsers: async (parent, args, context) => {
-			const users = await User.find().populate('friends');
+      throw new AuthenticationError('Not logged in');
+    },
+    allUsers: async (parent, args, context) => {
+      const users = await User.find().populate('friends');
 
-			return users;
-		},
-	},
-	Mutation: {
-		addUser: async (parent, args) => {
-			const user = await User.create(args);
-			const token = signToken(user);
+      return users;
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-			return { token, user };
-		},
+      return { token, user };
+    },
 
-		updateUser: async (parent, args, context) => {
-			const saltRounds = 10;
-			if (context.user) {
-				const validateUser = await User.findById(context.user._id);
-				const correctPw = await validateUser.isCorrectPassword(args.currentPassword);
+    updateUser: async (parent, args, context) => {
+      const saltRounds = 10;
+      if (context.user) {
+        const validateUser = await User.findById(context.user._id);
+        const correctPw = await validateUser.isCorrectPassword(args.currentPassword);
 
-				if (!correctPw) {
-					throw new AuthenticationError('Incorrect credentials');
-				}
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
 
-				const user = await User.findByIdAndUpdate(
-					context.user._id,
-					{
-						email: args.newEmail,
-						firstName: args.newId,
-						lastName: args.newNickname,
-						password: await bcrypt.hash(args.newPassword, saltRounds),
-					},
-					{ new: true }
-				);
+        const user = await User.findByIdAndUpdate(
+          context.user._id,
+          {
+            email: args.newEmail,
+            firstName: args.newId,
+            lastName: args.newNickname,
+            password: await bcrypt.hash(args.newPassword, saltRounds),
+          },
+          { new: true }
+        );
 
-				const token = signToken(user);
+        // const token = signToken(user);
 
-				return { token, user };
-			}
+        return { token, user };
+      }
 
-			throw new AuthenticationError('Not logged in');
-		},
+      throw new AuthenticationError('Not logged in');
+    },
 
-		login: async (parent, { email, password }) => {
-			const user = await User.findOne({ email });
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-			if (!user) {
-				throw new AuthenticationError('Incorrect credentials');
-			}
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
-			const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
-			if (!correctPw) {
-				throw new AuthenticationError('Incorrect credentials');
-			}
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
-			const token = signToken(user);
-			console.log(token);
+      const token = signToken(user);
+      console.log(token);
 
-			return { token, user };
-		},
+      return { token, user };
+    },
 
-		addFriend: async (parent, args) => {
-			const friend = User.findOneAndUpdate(
-				{ _id: args.userId },
-				{ $addToSet: { friends: args.friendId } },
-				{ new: true }
-			);
-		},
-		removeFriend: async (parent, args) => {
-			const friend = User.findOneAndUpdate(
-				{ _id: args.userId },
-				{ $pull: { friends: args.friendId } },
-				{ new: true }
-			);
-		},
-	},
+    addFriend: async (parent, args) => {
+      const friend = User.findOneAndUpdate(
+        { _id: args.userId },
+        { $addToSet: { friends: args.friendId } },
+        { new: true }
+      );
+    },
+    removeFriend: async (parent, args) => {
+      const friend = User.findOneAndUpdate(
+        { _id: args.userId },
+        { $pull: { friends: args.friendId } },
+        { new: true }
+      );
+    },
+  },
 };
 
 module.exports = resolvers;
